1ca2e21e90c52c1d7a642a4a0362228c913b911a
Quentin Zayats <qzayats1@binghamton.edu>
2020-10-28 16:51:54 -0400

    push maybe

---
+++ Project.py
@@ -0,0 +1,10 @@
+import pygame
+def printNames():
+    print("Quentin Zayats:Back End")
+    print("Emanuel Francisco:Front End")
+    print("Mark Lanning:Software Lead")
+
+def main():
+	printNames()
+	pygame.init()
+main()
